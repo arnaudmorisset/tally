68d3cc1588be636d31e4c13c6e9db4ff5d88c7b6
Arnaud Morisset <28533995+arnaudmorisset@users.noreply.github.com>
2024-02-21 17:24:05 +0100

    Create base program structure

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 )
 
 // printError prints the error to stderr.
 func printError(err error) {
-	fmt.Fprint(os.Stderr, err.Error())
+	fmt.Fprint(os.Stderr, err.Error()+"\n")
 }
 
 // print prints the arguments to stdout.
 func print(args ...interface{}) {
 	fmt.Println(args...)
+}
+
+type Count struct {
+	Words int `json:"words"`
+	Lines int `json:"lines"`
+	Bytes int `json:"bytes"`
 }
 
 func main() {
@@ -23,6 +31,54 @@ func main() {
 }
 
 func run() error {
-	print("Hello, World!")
+	outputFormat := flag.String("f", "text", "Output format: text, json")
+	flag.Parse()
+
+	// Read file content
+	filePath := flag.Arg(0)
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		err := fmt.Errorf("Error reading file: %v", err)
+		printError(err)
+		os.Exit(1)
+	}
+
+	// Perform counting
+	count := Count{
+		Words: countWords(content),
+		Lines: countLines(content),
+		Bytes: len(content),
+	}
+
+	switch *outputFormat {
+	case "json":
+		jsonOutput, _ := toJSON(count)
+		print(jsonOutput)
+	case "text":
+		fallthrough
+	default:
+		print("Words:", count.Words)
+		print("Lines:", count.Lines)
+		print("Bytes:", count.Bytes)
+	}
+
 	return nil
+}
+
+// To be implemented
+func countWords(content []byte) int {
+	return 0
+}
+
+// To be implemented
+func countLines(content []byte) int {
+	return 0
+}
+
+func toJSON(count Count) (string, error) {
+	data, err := json.Marshal(count)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
 }
